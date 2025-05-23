270858768ffecc966fa5fe79e4cf012016cd0270
stanli206 <stantilin@gmail.com>
2025-05-23 21:44:01 +0530

    latest commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoutes.js';
-import chatRoutes from './routes/chatRoutes.js';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import chatRoutes from "./routes/chatRoutes.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 dotenv.config();
 
@@ -17,8 +17,8 @@ const httpServer = createServer(app);
 // Socket.io setup
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-    methods: ['GET', 'POST'],
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    methods: ["GET", "POST"],
   },
 });
 
@@ -27,29 +27,48 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/chat', chatRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/chat", chatRoutes);
 
 // Socket.io connection
-io.on('connection', (socket) => {
-  console.log('A user connected');
+io.on("connection", (socket) => {
+  console.log("A user connected");
 
-  socket.on('join', (userId) => {
+  socket.on("join", (userId) => {
     socket.join(userId);
     console.log(`User ${userId} joined their room`);
   });
 
-  socket.on('sendMessage', (message) => {
-    io.to(message.receiver._id).emit('receiveMessage', message);
+  socket.on("sendMessage", (message) => {
+    // Broadcast to both sender and receiver rooms
+    io.to(message.sender._id)
+      .to(message.receiver._id)
+      .emit("receiveMessage", message);
   });
 
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
 });
+// io.on('connection', (socket) => {
+//   console.log('A user connected');
 
-const PORT = process.env.PORT || 5000;
+//   socket.on('join', (userId) => {
+//     socket.join(userId);
+//     console.log(`User ${userId} joined their room`);
+//   });
 
-httpServer.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+//   socket.on('sendMessage', (message) => {
+//     io.to(message.receiver._id).emit('receiveMessage', message);
+//   });
+
+//   socket.on('disconnect', () => {
+//     console.log('A user disconnected');
+//   });
+// });
+
+// const PORT = process.env.PORT || 5000;
+
+// httpServer.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
