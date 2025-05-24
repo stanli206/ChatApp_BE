9c5b1b588429e0bd7fe6caf06aa45ad8bdf59237
stanli206 <stantilin@gmail.com>
2025-05-24 20:05:40 +0530

    `Added server port configuration and logging, and commented out existing socket.io event listeners.`

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const io = new Server(httpServer, {
   },
 });
 
+const port = process.env.PORT || 5001;
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -30,6 +31,9 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
 
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
 // Socket.io connection
 io.on("connection", (socket) => {
   console.log("A user connected");
@@ -51,3 +55,25 @@ io.on("connection", (socket) => {
   });
 });
 
+// io.on('connection', (socket) => {
+//   console.log('A user connected');
+
+//   socket.on('join', (userId) => {
+//     socket.join(userId);
+//     console.log(`User ${userId} joined their room`);
+//   });
+
+//   socket.on('sendMessage', (message) => {
+//     io.to(message.receiver._id).emit('receiveMessage', message);
+//   });
+
+//   socket.on('disconnect', () => {
+//     console.log('A user disconnected');
+//   });
+// });
+
+// const PORT = process.env.PORT || 5000;
+
+// httpServer.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
